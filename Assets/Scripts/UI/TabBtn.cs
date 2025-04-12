598f2edea490f9c0a9d4231764c778faa99e8ed8
Juruohjr <45660518+juruohejiarui@users.noreply.github.com>
2025-04-13 00:32:43 +0800

    TEMP

--- Assets/Scripts/UI/TabBtn.cs
+++ Assets/Scripts/UI/TabBtn.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class TabBtn : MonoBehaviour
+public class TabBtn : MonoBehaviour, IPointerClickHandler
 {
+    private bool isSelected = false;
+
+    private Image selfImageComponent;
+    private Image iconComponent;
+
+    private TMP_Text textComponent;
+
+    private TabPanel belong;
+
+    public bool IsSelected {
+        get => isSelected; 
+        set {
+            isSelected = value;
+            if (isSelected) {
+                selfImageComponent.color = belong.SelectedColor;
+                if (iconComponent != null) iconComponent.color = belong.SelectedIconColor;
+                if (textComponent != null) textComponent.color = belong.SelectedIconColor;
+            } else {
+                selfImageComponent.color = belong.UnselectedColor;
+                if (iconComponent != null) iconComponent.color = belong.UnselectedIconColor;
+                if (textComponent != null) textComponent.color = belong.UnselectedIconColor;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        belong = GetComponentInParent<TabPanel>();
+        TabBtn[] btns = belong.GetComponentsInChildren<TabBtn>();
+        selfImageComponent = GetComponent<Image>();
+        iconComponent = transform.Find("icon").GetComponent<Image>();
+        textComponent = transform.Find("text").GetComponent<TMP_Text>();
         
+        IsSelected = btns[0] == this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        for (int i = 0; i < belong.tabBtns.Length; i++) {
+            if (belong.tabBtns[i] == this) {
+                belong.ChangeSelected(i);
+                break;
+            }
+        }
     }
 }
--- Assets/Scripts/UI/TabPanel.cs
+++ Assets/Scripts/UI/TabPanel.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UI;
 
 public class TabPanel : MonoBehaviour
 {
     public TabBtn[] tabBtns;
-    public TabBtn selectedTabBtn;
+    public TabBtn selectedTabBtn { get => tabBtns[selectedIndex]; }
+
+    public int selectedIndex;
+
+    public Color SelectedColor = Color.white;
+    public Color SelectedIconColor = Color.red;
+
+    public Color UnselectedColor = Color.red;
+    public Color UnselectedIconColor = Color.white;
     // Start is called before the first frame update
+
+    public void ChangeSelected(int index) {
+        selectedTabBtn.IsSelected = false;
+        selectedIndex = index;
+        selectedTabBtn.IsSelected = true;
+    }
     void Start()
     {
-        
+        tabBtns = GetComponentsInChildren<TabBtn>();
+        selectedIndex = 0;
     }
 
     // Update is called once per frame
